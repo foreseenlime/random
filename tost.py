d34f892afb2e0893d120c434503b9b4208f1ce04
foreseenlime <troyhkiwi@gmail.com>
2024-10-21 13:10:17 +1300

    added random test script

--- tost.py
+++ tost.py
@@ -5,3 +5,11 @@ a = 0
 b = ran.randint(1, 3)
 c = 69
 
+def dice():
+    for i in range(ran.randint(1, 6)):
+        print(i)
+
+while True:
+    a = input("sdnjflk jdfkl: ")
+    if a == "hello":
+        dice()
