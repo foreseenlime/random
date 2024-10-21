d53aea4676d5d7a3f5ddab6e49404088d04d9039
foreseenlime <troyhkiwi@gmail.com>
2024-10-22 09:04:03 +1300

    added comments for better clarity and changed the button you need to press to cancel a function for more consistency across the functions

--- mincro.py
+++ mincro.py
@@ -2,10 +2,17 @@ from time import *
 from random import *
 from microbit import *
 from music import *
+
+
 display.show(Image.HAPPY)
 
+
+#heart beating function
 def heart():
     while True:
+
+        #repeats 2 time:
+        #small heart, big heart, small heart
 
         for i in range(2):
             display.show(Image.HEART_SMALL)
@@ -14,37 +21,45 @@ def heart():
             sleep(150)
             display.show(Image.HEART_SMALL)
         
-        
+        #wait in between each 2 heart beats
         sleep(400)
 
-        if button_b.was_pressed() and button_a.was_pressed:
+        #checks if button was pressed to see if it needs to be cancelled
+        if button_b.was_pressed():
             display.clear()
             break
 
+
+#dice function
 def dice():
     display.show(Image.ANGRY)
     while True:
 
+        #if microbit was shaken, display random number
         if accelerometer.was_gesture('shake'):
             play(BA_DING)
             display.show(randint(1, 6))
-
-        # add an IF command that checks if it was shaken
-        # when it is shaken make it display a random number
         
-        if button_a.was_pressed() and button_b.was_pressed():
+        #checks if button was pressed to see if it needs to be cancelled
+        if button_b.was_pressed():
             break
 
+
+#steptracker function
 def steptracker():
+
+    #clear display and reset step count
     display.clear()
     steps = 0
     while True:
 
+        #if microbit was shaken (step taken) increase step count and display it
         if accelerometer.was_gesture('shake'):
             steps += 2
             display.show(steps)
 
-        if button_a.was_pressed():
+        #check if button was pressed to see if it needs to be cancelled
+        if button_b.was_pressed():
             display.clear()
             break
 
@@ -57,3 +72,15 @@ while True:
 
     if pin_logo.is_touched():
         pass
+
+
+
+
+
+#               IDEAS:                  #
+
+# -instead of the while True, make a function for the main script that can be run when you want to end a 
+#  function but break doesnt work
+
+# - make a function picker similar to what you had at the start, where pressing B changed a pixel on the display, 
+#   and pressing A picks the function
