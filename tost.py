7b5b010d42df211c58c80901b304c3030bf83e93
foreseenlime <troyhkiwi@gmail.com>
2024-10-22 08:18:33 +1300

    removed test code

--- tost.py
+++ tost.py
@@ -5,11 +5,3 @@ a = 0
 b = ran.randint(1, 3)
 c = 69
 
-def dice():
-    for i in range(ran.randint(1, 6)):
-        print(i)
-
-while True:
-    a = input("sdnjflk jdfkl: ")
-    if a == "hello":
-        dice()
