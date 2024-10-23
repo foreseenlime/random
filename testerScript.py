3869d93bc18c6fe7452b04d89bcb929fcb3c39be
t h <troyhkiwi@gmail.com>
2024-10-23 23:25:03 +0000

    got rid of unneeded script

--- testerScript.py
+++
@@ -1,9 +0,0 @@
-print('hi chat')
-
-a = input('test: ')
-
-if a == 'no':
-    print('reallycool_message.send()')
-
-else:
-    print('wrong answer')
--- tost.py
+++ tost.py
@@ -3,5 +3,5 @@ import tkinter as tk
 
 a = 0
 b = ran.randint(1, 3)
-c = 69
+c = 'hi chat'
 
