671511c7a3c4bb6eeb66b8d32955ac25c1b0fb30
foreseenlime <troyhkiwi@gmail.com>
2024-11-06 10:09:29 +1300

    tried things with pressing both buttons

--- at.py
+++ at.py
@@ -5,27 +5,31 @@ from random import *
 playerP = 2
 exPlayerP = 2
 
-bulletP = 2
-exBulletP = 2
+bulletP = 3
+exBulletP = 3
 
 display.set_pixel(2, 4, 9)
 
 while True:
 
-    if button_a.was_pressed():
+    if button_a.is_pressed():
         if playerP != 0:
             playerP -= 1
             display.set_pixel(playerP, 4, 9)
             display.set_pixel(exPlayerP, 4, 0)
             exPlayerP = playerP
+            sleep_ms(100)
 
-    if button_b.was_pressed():
+    if button_b.is_pressed():
         if playerP != 4:
             playerP += 1
             display.set_pixel(playerP, 4, 9)
             display.set_pixel(exPlayerP, 4, 0)
             exPlayerP = playerP
+            sleep_ms(100)
+
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.set_pixel(playerP, bulletP, 9)
 
     if pin_logo.is_touched():
-        for i in range(4):
-            pass
+        pass
