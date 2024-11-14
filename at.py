6cbc645b05769f62635fe6a9facb63915e6f7ebe
foreseenlime <troyhkiwi@gmail.com>
2024-11-15 10:06:22 +1300

    put moving and shooting in functions

--- at.py
+++ at.py
@@ -10,10 +10,10 @@ exBulletP = 3
 
 display.set_pixel(2, 4, 9)
 
-while True:
-
-    if button_a.is_pressed():
-        if playerP != 0:
+def moveLeft():
+    global playerP
+    global exPlayerP
+    if playerP != 0:
             playerP -= 1
             display.set_pixel(playerP, 4, 9)
             display.set_pixel(exPlayerP, 4, 0)
@@ -22,8 +22,10 @@ while True:
                 if button_a.is_pressed() == False:
                     break
 
-    if button_b.is_pressed():
-        if playerP != 4:
+def moveRight():
+    global playerP
+    global exPlayerP
+    if playerP != 4:
             playerP += 1
             display.set_pixel(playerP, 4, 9)
             display.set_pixel(exPlayerP, 4, 0)
@@ -32,22 +34,33 @@ while True:
                 if button_b.is_pressed() == False:
                     break
 
-    if button_a.is_pressed() and button_b.is_pressed():
+def shoot():
+    global bulletP
+    global exBulletP
+    global playerP
+    bulletP -= 1
+    display.set_pixel(playerP, bulletP, 9)
+    sleep_ms(100)
+    x = playerP
+    while bulletP != 0:
         bulletP -= 1
-        display.set_pixel(playerP, bulletP, 9)
-        sleep_ms(100)
-        x = playerP
-        while bulletP != 0:
-            bulletP -= 1
-            display.set_pixel(x, bulletP, 9)
-            display.set_pixel(x, exBulletP, 0)
-            exBulletP = bulletP
-            sleep_ms(100)
-
+        display.set_pixel(x, bulletP, 9)
         display.set_pixel(x, exBulletP, 0)
+        exBulletP = bulletP
+        sleep_ms(100)
 
-        bulletP = 4
-        exBulletP = 3
+    display.set_pixel(x, exBulletP, 0)
 
-    if pin_logo.is_touched():
-        pass
+    bulletP = 4
+    exBulletP = 3
+
+while True:
+
+    if button_a.is_pressed():
+        moveLeft()
+
+    if button_b.is_pressed():
+        moveRight()
+
+    if button_a.is_pressed() and button_b.is_pressed():
+        shoot()
